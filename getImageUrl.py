750580df6387b2e3f23961ab6d7b4e4bdc2468ed
can <canjia.mail@gmail.com>
2017-08-19 19:22:53 +0800

    add some annotation

--- getImageUrl.py
+++ getImageUrl.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python3.6
+'''
+for this script, you should run it using python3 with bs4 installed.
+python 2 is not supported, because the urllib has been changed between 2 and 3.
 
+you can use pip to install bs4
+
+$ pip3 install beautifulsoup4
+
+'''
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import sys
@@ -34,9 +42,23 @@ def findTargetUrl(url):
     return urlList
 
 if __name__ == '__main__':
+    '''
+    run this script use formate bellow:
+
+    $ python3 getImageUrl.py NAME TOTAL_NUM
     
+    NAME is the subtitle of https://images.qsfuli.com.
+    Now, there are 'qbyhx' 'wanimal' 'azhua' 'huafox' which you can choose
+    TOTAL_NUM is the number of pages you want to scrap.
+
+    For example:
+    $ python getImageUrl.py qbyhx 100
+
+    will scrap https://images.qsfuli.com/qbyhx/ for 0 - 100 pages.
+    
+    '''
     totalNum = int(sys.argv[2])
-    print (totalNum)
+    # print (totalNum)
     
     name = sys.argv[1]
     
